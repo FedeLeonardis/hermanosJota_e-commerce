3d9d3222e0c515ce1b0fb3574e120ef114b38e5d
tomaspicco <tomypicco@gmail.com>
2025-09-04 12:47:30 -0300

    Correción productos.html y productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,23 +1,3 @@
-// const catalogoContainer = document.getElementById("catalogo-container");
-
-// function renderCatalogo() {
-//   productos.forEach(prod => {
-//     const card = document.createElement("div");
-//     card.classList.add("card");
-//     card.innerHTML = `
-//       <img src="${prod.img}" alt="${prod.nombre}">
-//       <h3>${prod.nombre}</h3>
-//       <p>$${prod.precio}</p>
-//       <a href="producto.html?id=${prod.id}">Ver más</a>
-//     `;
-//     catalogoContainer.appendChild(card);
-//   });
-// }
-
-// if (catalogoContainer) renderCatalogo();
-
-// ../js/productos.js
-
 // Simula una carga asincrónica (como si fuera fetch)
 function obtenerProductos() {
   return new Promise((resolve) => {

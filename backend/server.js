9d98be007bed151370eb9d5b6af35053e2dfa95f
Walter Arias Molino <walteram.academico@gmail.com>
2025-10-28 18:45:20 -0300

    feat:conexion a BD y CRUD

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const cors = require("cors");
-const productosRouter = require("./routes/productosRoutes");
-const loggerMiddleware = require("./middleware/logger");
+const path = require("path");
+const mongoose = require("mongoose");
+
+//Cadena de conexión de la BD
+const DB_URI =
+  "mongodb+srv://walteramacademico_db_user:conectdatabase@cluster0.5alf9lx.mongodb.net/HermanosJotaDB?retryWrites=true&w=majority&appName=Cluster0";
+
+//Conexion de la BD
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log("Conexion a la BD exitosa"))
+  .catch(() => console.log("Error al conectar la BD"));
+
+// const productosRouter = require("./routes/productosRoutes");
+
+const productosRouter = require("./src/routes/productosRoutes");
+
+// const loggerMiddleware = require("./middleware/logger");
+const loggerMiddleware = require("./src/middleware/logger");
 
 // Puerto configurable mediante variable de entorno para facilitar despliegues.
 const puerto = process.env.PORT || 5000;
 
+//Permite servir las imagenes de los productos desde backend (si tienen dudas preguntar a Walter)
 const app = express();
+app.use(
+  "/images/productos",
+  express.static(path.join(__dirname, "images", "productos"))
+);
 
 // Middlewares globales: CORS, parsing de JSON y logging básico.
 app.use(cors());
 app.use(express.json());
 app.use(loggerMiddleware);
+
+//Ignora el error de favicon no encontrado
+app.get("/favicon.ico", (req, res) => res.status(204).end());
 
 // Endpoint raíz de cortesía para verificar que la API está viva.
 app.get("/", (req, res) => {
